371707135200df691e76b3b408f4add109e2001d
Stefan K <stefan.kikanovic@gmail.com>
2018-09-12 19:33:53 +0200

    bump

--- client/src/containers/new_case/NewCase.jsx
+++ client/src/containers/new_case/NewCase.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import NodesIcon from "grommet/components/icons/base/Nodes";
+import CloseIcon from "grommet/components/icons/base/Close";
+
+import { isEmpty } from "lodash";
 
 import {
   Header,
@@ -11,7 +14,9 @@ import {
   FormField,
   Distribution,
   Label,
-  Toast
+  Toast,
+  List,
+  ListItem
 } from "grommet";
 
 import PatientFileModal from "../../components/patient_file_modal/PatientFileModal";
@@ -29,7 +34,8 @@ class NewCase extends React.Component {
     super(props);
 
     this.state = {
-      newFileModalOpened: false
+      newFileModalOpened: false,
+      symptoms: []
     };
   }
 
@@ -55,7 +61,7 @@ class NewCase extends React.Component {
   };
 
   render() {
-    const { newFileModalOpened } = this.state;
+    const { newFileModalOpened, symptoms } = this.state;
     const { patientFile } = this.props;
 
     return (
@@ -83,58 +89,80 @@ class NewCase extends React.Component {
             />
           </Box>
         </Header>
-        <div className="file-btn-container">
-          {!patientFile.patient && (
-            <Button label="New File" onClick={this.toggleModal} />
-          )}
-        </div>
         <Row className="form-row-container">
           {patientFile.patient && (
             <Toast status="ok">Patient file successfuly created</Toast>
           )}
-          <Col md={4} />
+          <Col md={4}>
+            {!patientFile.patient && (
+              <Button label="New File" onClick={this.toggleModal} />
+            )}
+          </Col>
           <Col md={6} />
           <Col md={2}>
             {newFileModalOpened &&
               !patientFile.patient && (
                 <PatientFileModal closeModal={this.toggleModal} />
               )}
-            <FormField label="File ID">
-              <TextInput
-                id="item1"
-                name="item-1"
-                value="3631"
-                onDOMChange={this.onFileIdChange}
-              />
-            </FormField>
+            <TextInput
+              style={{
+                marginRight: "10px"
+              }}
+              placeHolder="File ID"
+              id="item1"
+              name="item-1"
+              onDOMChange={this.onFileIdChange}
+            />
           </Col>
         </Row>
-        <div className="illnesses-container">
-          <Label>Illnesses most likely associated with symptoms</Label>
-          <br />
-          <Distribution
-            series={[
-              {
-                label: "Fever",
-                value: 40
-              },
-              {
-                label: "Cold",
-                value: 30
-              },
-              {
-                label: "Kindies",
-                value: 20
-              },
-              {
-                label: "Heart Issue",
-                value: 10
-              }
-            ]}
-            units="%"
-            size="small"
-          />
-        </div>
+        <br />
+        <Row>
+          <Col md={12}>
+            <List>
+              <ListItem justify="between" separator="horizontal">
+                <span>Cough</span>
+                <span className="secondary">
+                  <CloseIcon
+                    onClick={() => console.log("Cough")}
+                    colorIndex="critical"
+                    style={{
+                      cursor: "pointer"
+                    }}
+                  />
+                </span>
+              </ListItem>
+            </List>
+          </Col>
+        </Row>
+        <br />{" "}
+        {!isEmpty(symptoms) && (
+          <div className="illnesses-container">
+            <Label>Illnesses most likely associated with symptoms</Label>
+            <br />
+            <Distribution
+              series={[
+                {
+                  label: "Fever",
+                  value: 40
+                },
+                {
+                  label: "Cold",
+                  value: 30
+                },
+                {
+                  label: "Kindies",
+                  value: 20
+                },
+                {
+                  label: "Heart Issue",
+                  value: 10
+                }
+              ]}
+              units="%"
+              size="small"
+            />
+          </div>
+        )}
       </div>
     );
   }
