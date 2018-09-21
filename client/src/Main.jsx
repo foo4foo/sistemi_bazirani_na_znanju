3d22efc95f8f3d9679453e817be74ffe23e349e4
stefan <stefannn.live@live.com>
2018-09-21 18:53:22 +0200

    Revert "layout grid fixed"
    This reverts commit ecc132b5a13c395aaf10013e43cdc8b03815af53.

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import {instanceOf} from "prop-types";
-import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import { instanceOf } from "prop-types";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
-import {withCookies, Cookies} from "react-cookie";
+import { withCookies, Cookies } from "react-cookie";
 
-import {Timeline} from "react-twitter-widgets";
-import {Row, Col} from "react-flexbox-grid";
-import {Navigation} from "./components/navigation/navigation";
+import { Timeline } from "react-twitter-widgets";
+import { Row, Col } from "react-flexbox-grid";
+import { Navigation } from "./components/navigation/navigation";
 
 import Login from "./components/session/login";
 import Patients from "./containers/patients";
@@ -25,37 +30,38 @@ class Main extends React.Component {
         <main>
           <Router>
             <Switch>
-              {localStorage.getItem("jwt")
-                ? (
-                  <Row>
-                    <Col lg={3} md={3}>
-                      <Navigation/>
+              {localStorage.getItem("jwt") ? (
+                <Row>
+                  <Navigation />
+                  <Row className="home-container">
+                    <Col md={3} className="twitter-container">
+                      <Timeline
+                        dataSource={{
+                          sourceType: "profile",
+                          screenName: "health"
+                        }}
+                        options={{
+                          username: "TwitterDev"
+                        }}
+                        onLoad={() => console.log("Timeline is loaded!")}
+                      />
                     </Col>
-                    <Col lg={9} md={9}>
-                      <Row>
-                        <Col md={3} className="twitter-container">
-                          <Timeline
-                            dataSource={{
-                            sourceType: "profile",
-                            screenName: "health"
-                          }}
-                            options={{
-                            username: "TwitterDev"
-                          }}
-                            onLoad={() => console.log("Timeline is loaded!")}/>
-                        </Col>
-                        <Col md={9}>
-                          <Route exact path="/" component={Home}/>
-                          <Route exact path="/patients" component={Patients}/>
-                          <Route exact path="/new_case" component={NewCase}/>
-                          <Route exact path="/intesive_care" component={IntesiveCare}/>
-                        </Col>
-                      </Row>
+                    <Col md={9}>
+                      <Route exact path="/" component={Home} />
+                      <Route exact path="/patients" component={Patients} />
+                      <Route exact path="/new_case" component={NewCase} />
+                      <Route
+                        exact
+                        path="/intesive_care"
+                        component={IntesiveCare}
+                      />
                     </Col>
                   </Row>
-                )
-                : (<Route exact path="/login" component={Login}/>)}
-              {!localStorage.getItem("jwt") && <Redirect to="/login"/>}
+                </Row>
+              ) : (
+                <Route exact path="/login" component={Login} />
+              )}
+              {!localStorage.getItem("jwt") && <Redirect to="/login" />}
             </Switch>
           </Router>
         </main>
